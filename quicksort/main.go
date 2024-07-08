dca96f0fef6698beb0d274344156a10254e8bbb8
George Dan Zbutea <george.zbutea@grabtaxi.com>
2024-07-08 16:13:32 +0300

    generic quicksort ⚡

--- quicksort/main.go
+++ quicksort/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"math/rand"
 )
 
-func quicksort(slice []int) []int {
+func quicksort[T cmp.Ordered](slice []T) []T {
 	if len(slice) < 2 {
 		return slice
 	}
 
 	pivotIdx := rand.Intn(len(slice))
-	var less, greater []int
+	var less, greater []T
 
 	for i, el := range slice {
 		if i == pivotIdx {
@@ -28,11 +29,18 @@ func quicksort(slice []int) []int {
 }
 
 func main() {
-	slice := make([]int, 10000)
+	intSlice := make([]int, 10000)
 
-	for i := range slice {
-		slice[i] = rand.Intn(10000)
+	for i := range intSlice {
+		intSlice[i] = rand.Intn(10000)
 	}
 
-	fmt.Println(quicksort(slice))
+	// fmt.Println(quicksort(intSlice))
+
+	stringSlice := []string{"orange", "kiwi", "grape", "mango", "banana",
+		"raspberry", "cherry", "fig", "elderberry", "lemon",
+		"honeydew", "tangerine", "papaya", "strawberry", "iceberg",
+		"jackfruit", "apple", "quince", "nectarine", "date"}
+
+	fmt.Println(quicksort(stringSlice))
 }
